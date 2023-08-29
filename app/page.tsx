595d3edad549cb5503bc4ca505d1853aa1adaffe
rujal <rujalbangdiwala@gmail.com>
2023-08-30 00:30:46 +0530

    text

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Logo from "@/components/logo";
-import Background
- from "@/components/background";
+import Background from "@/components/background";
+
 export default function Home() {
   return (
     <main className="absolute flex items-center justify-center h-[100vh] w-[100vw]">
       <Background/>
       <Logo />
+     
     </main>
   );
 }
--- components/logo.tsx
+++ components/logo.tsx
@@ -1,19 +1,50 @@
 "use client";
-
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import logo from "@/public/assets/codechef_logo.svg";
 import { motion } from "framer-motion";
 
 export default function Logo() {
+  const [animationComplete, setAnimationComplete] = useState(false);
+
+  const logoAnimation = {
+    initial: { y: 0 },
+    animate: { y: -600 },
+    transition: { duration: 20, delay: 30 },
+  };
+
+  const handleAnimationComplete = () => {
+    setAnimationComplete(true);
+  };
+
+  const textVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
-    <motion.div
-      className="z-10"
-      initial={{ y: 0}}
-      animate={{ y:-600}}
-      transition={{duration: 0.5, delay: 1.5}}
-    >
-      <Image src={logo} alt="logo" className="h-[20rem] w-[20rem]" />
-    </motion.div>
+    <div className=" fixed flex justify-center items-center h-screen">
+      <motion.div
+        className="z-1"
+        variants={logoAnimation}
+        initial="initial"
+        animate="animate"
+        transition="transition"
+        onAnimationComplete={handleAnimationComplete}
+      >
+        <Image src={logo} alt="logo" className="h-[20rem] w-[20rem]" />
+      </motion.div>
+
+      {animationComplete && (
+        <motion.div
+          className="fixed  text-center text-5xl uppercase mb-2"
+          variants={textVariants}
+          initial="hidden"
+          animate="visible"
+        >
+         welcome to code chef vit 
+        </motion.div>
+      )}
+    </div>
   );
 }
