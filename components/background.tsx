20af4437d0c8ca7f387ef3854e091b613374d358
Abh1noob <abhinavganeshank@gmail.com>
2023-08-31 20:17:53 +0530

    Added Scrollbar to Right Div

--- components/background.tsx
+++ components/background.tsx
@@ -3,15 +3,14 @@ import React, { useState, useEffect } from "react";
 import Logo from "./logo";
 import { motion, useAnimation } from "framer-motion";
 
-
-  export default function Background() {
-    const [scrollPosition, setScrollPosition] = useState(0);
-    const controls = useAnimation();
-    useEffect(() => {
-      const handleScroll = () => {
-        setScrollPosition(window.scrollY);
-      };
-      window.addEventListener("scroll", handleScroll);
+export default function Background() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const controls = useAnimation();
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
+    };
+    window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
@@ -24,84 +23,71 @@ import { motion, useAnimation } from "framer-motion";
       } else {
         controls.start({ x: 705 });
       }
-    }, 5000); 
+    }, 5000);
 
     return () => {
       clearTimeout(animationTimeout);
     };
   }, [scrollPosition, controls]);
 
-  return (
-    <>
-    <div>
-      {/* Left Div */}
-      <motion.div
-        className="absolute flex items-center justify-center h-[100vh] w-[100vw] bg-[#2c0ac4]"
-        initial={{ width: "100vw" }}
-        animate={{ width: "50vw" }}
-        transition={{ delay: 3 }}
-      >
-        <Logo />
-      </motion.div>
+    return (
+      <div className="h-[100vh] flex flex-row">
 
-      {/* Right Div */}
-      <div className="h-[100vh] w-[50vw] bg-black ml-[50vw] flex flex-col gap-[2vh] p-[1rem]">
-        <motion.div
-          className="h-[48vh] w-[48vw] bg-[#01ff34]"
-          initial={{ x: 705 }}
-          animate={{ x: 0 }}
-          transition={{
-            type: "just",
-            delay: 3.1,
-          }}
-        ></motion.div>
+        <div className="h-[100vh] w-[50vw] overflow-auto">
+          {/* Left Div */}
+          <motion.div
+            className="absolute flex flex-col items-center justify-center h-[100vh] w-[100vw] bg-[#2c0ac4]"
+            initial={{ width: "100vw" }}
+            animate={{ width: "50vw" }}
+            transition={{ delay: 3 }}
+          >
+            <Logo />
+          </motion.div>
 
-        <motion.div
-          className="h-[48vh] w-[48vw] bg-[#01ff34]"
-          initial={{ x: 705 }}
-          animate={{ x: 0 }}
-          transition={{
-            type: "just",
-            delay: 3.2,
-          }}
-        ></motion.div>
+        </div>
+
+        {/* Right Div */}
+        <div className="h-[100vh] w-[50vw] bg-black  p-[1rem] overflow-y-auto">
+          <motion.div
+            className="h-[30rem] w-[45vw] bg-[#ff0101] m-[1rem]"
+            initial={{ x: 705 }}
+            animate={{ x: 0 }}
+            transition={{
+              type: "just",
+              delay: 3.1,
+            }}
+          ></motion.div>
+
+          <motion.div
+            className="h-[30rem] w-[45vw] bg-[#fff701] m-[1rem]"
+            initial={{ x: 705 }}
+            animate={{ x: 0 }}
+            transition={{
+              type: "just",
+              delay: 3.2,
+            }}
+          ></motion.div>
+
+          <motion.div
+            className="h-[30rem] w-[45vw] bg-[#01ffea] m-[1rem]"
+            initial={{ x: 705 }}
+            animate={{ x: 0 }}
+            transition={{
+              type: "just",
+              delay: 3.2,
+            }}
+          ></motion.div>
+
+          <motion.div
+            className="h-[30rem] w-[45vw] bg-[#ff01ee] m-[1rem]"
+            initial={{ x: 705 }}
+            animate={{ x: 0 }}
+            transition={{
+              type: "just",
+              delay: 3.2,
+            }}
+          ></motion.div>
+        </div>
       </div>
-    </div>
-    <div>
-      {/* Left Div */}
-      <motion.div
-        className="absolute flex items-center justify-center h-[100vh] w-[100vw] bg-[#2c0ac4] text-5xl uppercase text-center "
-        initial={{ width: "100vw" }}
-        animate={{ width: "50vw" }}
-        transition={{ delay: 3}}
-      >
-       Welcome to Code Chef VIT
-      </motion.div>
-
-      {/* Right Div */}
-      <div className="h-[100vh] w-[50vw] bg-black ml-[50vw] flex flex-col gap-[2vh] p-[1rem]">
-        <motion.div
-          className="h-[48vh] w-[48vw] bg-[#01ff34]"
-          initial={{ x: 705 }}
-          animate={{ x: 0 }}
-          transition={{
-            type: "just",
-            delay: 3.0,
-          }}
-        ></motion.div>
-
-        <motion.div
-          className="h-[48vh] w-[48vw] bg-[#01ff34]"
-          initial={{ x: 705 }}
-          animate={{ x: 0 }}
-          transition={{
-            type: "just",
-            delay: 3.0,
-          }}
-        ></motion.div>
-      </div>
-    </div>
-    </>
-  );
-}
-
+    );
+  }
